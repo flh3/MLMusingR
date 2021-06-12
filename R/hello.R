042f23f85ec390416f92ff7c066d50b4b8f34867
Francis Huang <huangf@missouri.edu>
2021-06-12 16:44:11 -0500

    test

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' This is a test
+#'
+#' @description This is a test
+#' @param x what you enter
+#'
+#' @return whatever
+#' @export
+#'
+#' @examples
+#' hello('world')
+hello <- function(x) {
+  paste0("Hello, world!", x)
+}
