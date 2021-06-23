0eb1cb660bef75a1513408cc47321b019424161f
Francis Huang <flh3@hotmail.com>
2021-06-23 08:14:33 -0500

    data

---
+++ R/suspend.R
@@ -0,0 +1,18 @@
+#' Suspension data from Virginia
+#'
+#' Data from 8465 students from 100 schools in
+#' Virginia
+#' @usage data(suspend)
+#' @format Dataset:
+#' \describe{
+#'   \item{school}{School identifier}
+#'   \item{pminor}{Percent minority enrollment at school}
+#'   \item{male}{1 = male, 0 = female}
+#'   \item{sus}{Whether the student was suspended (1 = yes) in the
+#'   school year or not (0 = no). Self reported.}
+#'   \item{fight}{If the student got into one or more fights (1 = yes) in
+#'   the school year}
+#'   \item{gpa}{Students self-reported GPA; 1 = D to 4 = A}
+#'
+#' }
+"suspend"
