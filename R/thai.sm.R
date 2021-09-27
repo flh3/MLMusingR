ac8523ecfdaa91f281694837e85c74e780d76f21
Francis Huang <flh3@hotmail.com>
2021-09-27 13:06:08 -0500

    thai.sm
    update

---
+++ R/thai.sm.R
@@ -0,0 +1,16 @@
+#' Thai data from PISA (reduced)
+#'
+#' Example data to be used for centering
+#' @usage data(thai)
+#' @format A data frame with 4271 rows and 7 variables:
+#' \describe{
+#'   \item{math}{First plausible value in mathematics.}
+#'   \item{escs}{Index of economic, social, and cultural status.}
+#'   \item{intmat}{Mathematics interest.}
+#'   \item{schoolid}{School identifier}
+#'   \item{othl}{Spoke another language at home other than Thai. 1 = yes, 0 = no.}
+#'   \item{private}{Private school. 1 = yes, 0 = no.}
+#'   \item{schsize}{Total school enrolment.}
+#' }
+#' @source \url{https://gpseducation.oecd.org/CountryProfile?primaryCountry=THA&treshold=10&topic=PIf}
+"thai.sm"
