1294a4a15b1c71deacd3dad813273b4abe3dc181
Francis Huang <flh3@hotmail.com>
2021-10-11 10:11:04 -0500

    ri

---
+++ R/ri_test1.R
@@ -0,0 +1,12 @@
+#' Sample dataset 1 for testing the likelihood ratio test
+#'
+#' Example data for testing the need for a random intercept. Illustrates the need to adjust the p values for a modified LRT.
+#' @usage data(ri_test1)
+#' @format A data frame with 900 observations from 30 groups and 4 variables:
+#' \describe{
+#'   \item{y}{The outcome variable.}
+#'   \item{w1}{A level-2 predictor.}
+#'   \item{x1}{A level-1 predictor}
+#'   \item{group}{The cluster identifier}
+#' }
+"ri_test1"
---
+++ R/ri_test2.R
@@ -0,0 +1,12 @@
+#' Sample dataset 2 for testing the likelihood ratio test (LRT)
+#'
+#' Example data for testing the need for a random intercept. LRT results show that a random slope is not warranted.
+#' @usage data(ri_test2)
+#' @format A data frame with 3,000 observations from 30 groups and 4 variables:
+#' \describe{
+#'   \item{y}{The outcome variable.}
+#'   \item{w1}{A level-2 predictor.}
+#'   \item{x1}{A level-1 predictor}
+#'   \item{group}{The cluster identifier}
+#' }
+"ri_test2"
